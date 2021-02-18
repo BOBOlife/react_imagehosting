56c32d9c5a8496dc51c9607ec852ae5cb75123f9
wangbo <bbw1996@qq.com>
2021-02-18 17:42:48 +0800

    构建注册页面，实现表单验证

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,22 +8,22 @@ const Wrapper = styled.div`
   border-radius: 4px;
   padding: 20px;
 `
-const Title =styled.h1`
+const Title = styled.h1`
   text-align: center;
   margin-bottom: 30px;
 `
 const layout = {
   labelCol: {
-    span: 4,
+    span: 6,
   },
   wrapperCol: {
-    span: 20,
+    span: 18,
   },
 }
 const tailLayout = {
   wrapperCol: {
-    offset: 4,
-    span: 20,
+    offset: 6,
+    span: 18,
   },
 }
 
@@ -35,7 +35,20 @@ const Component = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+  const validateUsername = (rule, value) => {
+    if (/\W/.test(value)) return Promise.reject('不能出现字母数字下划线以外的字符')
+    if (value.length < 4 || value.length > 10) return Promise.reject('用户名长度为4~10个字符')
+    return Promise.resolve()
+  }
 
+  const validateConfirm = ({getFieldValue}) => ({
+    validator(rule, value) {
+      if (!value || getFieldValue('password') === value) {
+        return Promise.resolve()
+      }
+      return Promise.reject('两次密码不匹配')
+    }
+  })
   return (
     <Wrapper>
       <Title>注册</Title>
@@ -53,6 +66,9 @@ const Component = () => {
               required: true,
               message: '输入用户名',
             },
+            {
+              validator: validateUsername
+            }
           ]}
         >
           <Input />
@@ -66,18 +82,27 @@ const Component = () => {
               required: true,
               message: '输入密码',
             },
+            {
+              min: 4,
+              message: '最少输入4个字符'
+            },
+            {
+              max: 10,
+              message: '最大输入10个字符'
+            }
           ]}
         >
           <Input.Password />
         </Form.Item>
         <Form.Item
           label="确认密码"
-          name="password"
+          name="confirmPassword"
           rules={[
             {
               required: true,
               message: '再次确认密码',
             },
+            validateConfirm
           ]}
         >
           <Input.Password />
